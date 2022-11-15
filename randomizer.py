2896ff6473ba0c0c5d7f1f5d61acffa0d02766a1
Kevin Montero <69410099+kdmontero@users.noreply.github.com>
2022-11-15 14:53:47 +0800

    added a simplified version

--- randomizer.py
+++ randomizer.py
@@ -2,6 +2,7 @@ from openpyxl import Workbook, load_workbook
 from openpyxl.worksheet.worksheet import Worksheet
 import random
 
+# use this when reading workbook thru the script is available
 def execute() -> None:
     NUMBER_OF_CATEGORIES = 62
 
@@ -33,5 +34,29 @@ def execute() -> None:
     print(random_questions_indices)
 
     
+# use this when executing the script in browser and reading a workbook is not possible
+def execute_simple_script() -> None:
+    NUMBER_OF_CATEGORIES = 62
+
+    categories = [] # Place here the list of categories for reference only. Not required in running the script
+    database_count = [] # Place here the total number of questions in database per categories (list of numbers)
+    questions_count = [] # Place here the required number of questions (list of numbers)
+
+    random_questions_indices = []
+    current_index = 1
+
+    for required, total in zip(questions_count, database_count):
+        indices = sorted(random.sample(
+            range(current_index, current_index + total), required
+        ))
+
+        random_questions_indices += indices
+        current_index += total
+
+    print(random_questions_indices)
+    random.shuffle(random_questions_indices)
+    print(random_questions_indices)
+    
+    
 if __name__ == '__main__':
-    execute()
+    execute_simple_script()
